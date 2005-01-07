ded90f7c70dfc3b357148c5d906f30122b7d0e06
zarzycki <zarzycki@e92bca22-270c-0410-9cea-e3f1106b6a1c>
2005-01-07 00:47:12 +0000

    <rdar://problem/3941755> LDAP startup item could be removed
    git-svn-id: http://svn.macosforge.org/repository/launchd/trunk@20145 e92bca22-270c-0410-9cea-e3f1106b6a1c

--- launchd/src/StartupItems.c
+++ launchd/src/StartupItems.c
@@ -191,6 +191,7 @@ SpecialCasesStartupItemHandler(CFMutableDictionaryRef aConfig)
 		CFSTR("DirectoryServices"),
 		CFSTR("Network Configuration"),
 		CFSTR("mDNSResponder"),
+		CFSTR("LDAP"),
 		NULL
 	};
 	CFMutableArrayRef aList, aNewList;
