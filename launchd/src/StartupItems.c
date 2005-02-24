502c057da2bd484453a6443af502ce1d14eb63ad
zarzycki <zarzycki@e92bca22-270c-0410-9cea-e3f1106b6a1c>
2005-02-24 23:54:36 +0000

    Delete a dead StartupItem.
    git-svn-id: http://svn.macosforge.org/repository/launchd/trunk@20297 e92bca22-270c-0410-9cea-e3f1106b6a1c

--- launchd/src/StartupItems.c
+++ launchd/src/StartupItems.c
@@ -193,6 +193,7 @@ SpecialCasesStartupItemHandler(CFMutableDictionaryRef aConfig)
 		CFSTR("mDNSResponder"),
 		CFSTR("Cron"),
 		CFSTR("Core Graphics"),
+		CFSTR("Network"),
 		NULL
 	};
 	CFMutableArrayRef aList, aNewList;
