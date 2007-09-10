fa5e5d7406cb5821294d5bed0d01014949a7b553
zarzycki@apple.com <zarzycki@apple.com@e92bca22-270c-0410-9cea-e3f1106b6a1c>
2007-09-10 18:38:14 +0000

    Better error reporting.
    git-svn-id: http://svn.macosforge.org/repository/launchd/trunk@23365 e92bca22-270c-0410-9cea-e3f1106b6a1c

--- launchd/src/launchd_runtime.c
+++ launchd/src/launchd_runtime.c
@@ -1269,7 +1269,7 @@ runtime_log_uncork_pending_drain(void)
 	tmp_port = drain_reply_port;
 	drain_reply_port = MACH_PORT_NULL;
 
-	if (!launchd_assumes(job_mig_log_drain_reply(tmp_port, 0, outval, outvalCnt) == 0)) {
+	if (!launchd_assumes((errno = job_mig_log_drain_reply(tmp_port, 0, outval, outvalCnt)) == 0)) {
 		launchd_assumes(launchd_mport_deallocate(tmp_port) == KERN_SUCCESS);
 	}
 
