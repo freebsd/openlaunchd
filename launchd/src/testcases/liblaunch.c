598adac7b422d70b097d1a748d1ed10dee535bf7
R. Tyler Ballance <tyler@grape.local>
2008-09-27 23:20:30 -0700

    Update test case for liblaunch, add temporary Makefile

--- launchd/src/testcases/liblaunch.c
+++ launchd/src/testcases/liblaunch.c
@@ -11,17 +11,14 @@
 #include "config.h"
 #include "liblaunch_public.h"
 #include "liblaunch_private.h"
-//#include "liblaunch_internal.h"
+#include "liblaunch_internal.h"
 #include "launchd_ktrace.h"
-
-void include_file_test() 
-{
-	CU_PASS("Passing empty test, if this test compiled, our includes are set");
-}
 
 void launch_data_new_opaque_test()
 {
-	CU_PASS("Passing empty test");
+	const void *o = NULL;
+	launch_data_t op = launch_data_new_opaque(o, sizeof((1024 * sizeof(char))));
+	CU_ASSERT_PTR_NOT_NULL(op);
 }
 
 int main(int argc, void *argv)
@@ -30,13 +27,12 @@ int main(int argc, void *argv)
 	if (CUE_SUCCESS != CU_initialize_registry())
 		return CU_get_error();
 	
-	tests = CU_add_suite("Liblaunch Tests", NULL, NULL);
+	tests = CU_add_suite("Liblaunch API Tests", NULL, NULL);
 	if (tests == NULL) {
 		CU_cleanup_registry();
 		return CU_get_error();
 	}
 
-	CU_add_test(tests, "Compile Test", include_file_test);
 	CU_add_test(tests, "launch_data_new_opaque", launch_data_new_opaque_test);
 
 	CU_basic_set_mode(CU_BRM_VERBOSE);
