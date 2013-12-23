31073c7a6de23a0a5950a1c6b7c090dab0f11584
R. Tyler Croy <tyler@monkeypox.org>
2013-12-23 11:24:11 -0800

    Define DEBUG for builds right now

--- launchd.prog.mk
+++ launchd.prog.mk
@@ -1,8 +1,10 @@
 # $FreeBSD$
 
+MAINTAINER=tyler@freebsd.org
 
 CFLAGS+=-g -I${PWD}/../launchd \
 		-I${PWD}/../liblaunch \
-		-I/usr/local/include
+		-I/usr/local/include \
+		-DDEBUG
 
 .include <bsd.prog.mk>
