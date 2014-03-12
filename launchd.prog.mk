b28fe408f69e1ac416cb1b9131d41e0f46542376
R. Tyler Croy <tyler@monkeypox.org>
2014-03-11 17:06:24 -0700

    Add more CFLAGS and warnings to help track down bad code

--- launchd.prog.mk
+++ launchd.prog.mk
@@ -2,9 +2,21 @@
 
 MAINTAINER=tyler@freebsd.org
 
-CFLAGS+=-g -I${PWD}/../launchd \
+CFLAGS+=-g -fPIC -fno-exceptions -fstack-protector -fvisibility=hidden
+
+# WARNINGS
+CFLAGS+=-Wall -Wextra -Wno-unused-parameter \
+		-Wno-unused-label -Wformat -Wreturn-type \
+		-Wsign-compare -Wmultichar -Winit-self \
+		-Wuninitialized -Wno-deprecated -Wformat-security \
+		-Werror
+
+# INCLUDES
+CFLAGS+=-I${PWD}/../launchd \
 		-I${PWD}/../liblaunch \
-		-I/usr/local/include \
-		-DDEBUG
+		-I/usr/local/include
+
+# DEFINES
+CFLAGS+=-DDEBUG
 
 .include <bsd.prog.mk>
